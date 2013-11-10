90d9f6c722c4842e90a544257e341f17b87192af
Iwan Sunarya <ones006@gmail.com>
2013-11-10 22:22:12 +0700

    home

--- themes/tetuka/index.php
+++ themes/tetuka/index.php
@@ -133,15 +133,7 @@
 					
 					
 					<!-- Pagination -->
-					<!-- <ul class="pagination">
-						<li><a href="#">&laquo;</a></li>
-						<li><a href="#">1</a></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#">4</a></li>
-						<li><a href="#">5</a></li>
-						<li><a href="#">&raquo;</a></li>
-					</ul> -->
+					<!--  -->
 				</div>
 				<!-- Add the extra clearfix for only the required viewport -->
 				<div class="clearfix visible-xs"></div>
